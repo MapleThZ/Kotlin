b376fcbf189a122b944e35051e7d81984ee2e511
PiyanatK <31474579+MapleThZ@users.noreply.github.com>
2024-12-18 08:45:16 +0700

    Delete bin directory

--- bin/main/com/kotlin/library/repositories/CustomerRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlin.library.repositories
-
-import org.springframework.data.jpa.repository.JpaRepository
-import com.kotlin.library.entities.Customer
-
-interface CustomerRepository : JpaRepository<Customer, Long> {
-    fun findByfirstName(firstName: String): Customer?
-    fun findById(id: String): Customer?
-}
